77dc0002915b5d88d55c2ff00f19e3c643f964f8
root <root@DESKTOP-ISQ43BP>
2023-01-05 13:17:15 -0500

    upgrade

--- add.c
+++ add.c
@@ -3,7 +3,7 @@
 /**
  * add_node - add a newnode at beginning of our
  * linked-listed behavior stack
- * @stack: double pointer to head of stack
+ * @head: Pointer to pointer of head node of the list
  * @n: new data to fill in new node
  * Return: pointer to new node
  */
@@ -22,13 +22,17 @@ stack_t *add_node(stack_t **head, const int n)
 	if (head && *head)
 	{
 		(*head)->prev = new;
+		
 		new->next = (*head);
 	}
+
 	else
 		new->next = NULL;
 	(*head) = new;
+
 	return (new);
 }
+
 /**
  * check_digit - check if the string contains only digits
  * @arg: pointer on argument to check
@@ -46,13 +50,14 @@ int check_digit(char *arg)
 		if (_isdigit(arg[i]) == 0)
 			return (1);
 	}
+
 	return (0);
 }
 /**
-* _isdigit -  prints if the character is a digit.
-* @c: character to check.
-* Return: 0 if exited correctly, non-zero otherwise.
-*/
+ * _isdigit -  prints if the character is a digit.
+ * @c: character to check.
+ * Return: 0 if exited correctly, non-zero otherwise.
+ */
 
 int _isdigit(int c)
 {
