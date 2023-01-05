f436569e42d5ca9aa5ad7f84ee5f84ed1c6e33f1
root <root@DESKTOP-ISQ43BP>
2023-01-05 12:56:10 -0500

    upgrade

--- functions.c
+++ functions.c
@@ -1,32 +1,22 @@
 #include "monty.h"
 
-/**
- * push_func - pushes an element to the stack
- * @stack: double pointer to a double linked list as stack
- * @line_number: number of lines inside the opcode file
- * Return: void
- */
 void _push(stack_t **stack, unsigned int line_number)
 {
-	stack_t *push_element;
-	(void) line_number;
+	int add;
+	char *value;
 
-	push_element = malloc(sizeof(stack_t));
-	if (push_element == NULL)
+	value = strtok(NULL, "\n\t ");
+	if (value == NULL || check_digit(value))
+	{
+		fprintf(stderr, "L%u: usage: push integer\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	add = atoi(value);
+	if (add_node(stack, add) == NULL)
 	{
 		fprintf(stderr, "Error: malloc failed\n");
 		exit(EXIT_FAILURE);
 	}
-	push_element->n = global_var;
-	if (*stack == NULL)
-		push_element->next = NULL;
-	else
-		push_element->next = *stack;
-	push_element->prev = NULL;
-	*stack = push_element;
-	if (push_element->next != NULL)
-		push_element->next->prev = push_element;
-
 }
 
 void _pall(stack_t **stack, unsigned int line_number)
