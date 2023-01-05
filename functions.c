ffdab79d17f52135f998a90ce505a721ceef6ed6
root <root@DESKTOP-ISQ43BP>
2023-01-05 12:12:11 -0500

    upgrade

--- functions.c
+++ functions.c
@@ -1,6 +1,6 @@
 #include "monty.h"
 
-void push(stack_t **stack, unsigned int line_number)
+void _push(stack_t **stack, unsigned int line_number)
 {
 	int add;
 	char *value;
@@ -19,7 +19,7 @@ void push(stack_t **stack, unsigned int line_number)
 	}
 }
 
-void pall(stack_t **stack, unsigned int line_number)
+void _pall(stack_t **stack, unsigned int line_number)
 {
 	stack_t *temp;
 
--- monty.h
+++ monty.h
@@ -39,8 +39,8 @@ typedef struct instruction_s
 	void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
 
-void push(stack_t **stack, unsigned int line_number);
-void pall(stack_t **stack, unsigned int line_number);
+void _push(stack_t **stack, unsigned int line_number);
+void _pall(stack_t **stack, unsigned int line_number);
 int main(int argc, char *argv[]);
 void free_stack(stack_t *head);
 void get_func(char *op, stack_t **stack, unsigned int line_number);
