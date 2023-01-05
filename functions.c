8103a82d6992d8ae6369d0777455aa0953e01406
root <root@DESKTOP-ISQ43BP>
2023-01-05 13:20:15 -0500

    upgrade

--- functions.c
+++ functions.c
@@ -76,3 +76,21 @@ void get_func(char *op, stack_t **stack, unsigned int line_number)
 	fprintf(stderr, "L%u: unknown instruction %s\n", line_number, op);
 	exit(EXIT_FAILURE);
 }
+
+#include "monty.h"
+/**
+ * pint_func - prints the value at the top of the stack
+ * @stack: double pointer to the beggining of the stack (DLL)
+ * @line_number: the numbers of lines in the opcode file
+ * Return: void
+ */
+void pint_func(stack_t **stack, unsigned int line_number)
+{
+	if (*stack == NULL)
+	{
+		fprintf(stderr, "L%d: can't pint, stack empty\n", line_number);
+		error = 1;
+		return;
+	}
+	fprintf(stdout, "%d\n", (*stack)->n);
+}
